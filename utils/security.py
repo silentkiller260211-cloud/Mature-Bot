55db7e551efae7717dc1aadce72b7e978c627a5f
silentkiller260211-cloud <silentkiller260211@gmail.com>
2026-07-03 15:46:42 +0530

    Delete utils/security.py

--- utils/security.py
+++
@@ -1,7 +0,0 @@
-import os
-
-OWNER_ID = int(os.getenv("OWNER_ID", 0))
-DEVELOPER_ID = int(os.getenv("DEVELOPER_USER_ID", 0))
-
-def is_owner(user_id):
-    return user_id == OWNER_ID or user_id == DEVELOPER_ID
